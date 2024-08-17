bbf1303c4695931467bc0c50f93cc2522e02293b
Gustavo Israel Londa <gustavo.londa@inovtech.com.mx>
2024-08-16 23:18:40 -0500

    Se agrego la entidad generica que se va usar

--- backend/auth-service/src/main/java/com/smartjob/authservice/commons/api/infraestructure/entities/BaseEntity.java
+++ backend/auth-service/src/main/java/com/smartjob/authservice/commons/api/infraestructure/entities/BaseEntity.java
@@ -1,4 +1,31 @@
 package com.smartjob.authservice.commons.api.infraestructure.entities;
+import java.util.Date;
 
-public class BaseEntity {
+
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+@SuperBuilder(toBuilder = true)
+public abstract  class BaseEntity {
+    @CreationTimestamp
+    @Column(name = "created" , columnDefinition = "TIMESTAMP")
+    private Date created;
+
+    @UpdateTimestamp
+    @Column(name = "modified" , columnDefinition = "TIMESTAMP")
+    private Date modified;
+
+    private boolean isActive = Boolean.TRUE;
 }
