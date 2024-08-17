12f163a448b0eaae9741955113b863578571b67c
Gustavo Israel Londa <gustavo.londa@inovtech.com.mx>
2024-08-16 23:15:04 -0500

    Se agrego la configuracion de multilenguaje

--- backend/auth-service/src/main/java/com/smartjob/authservice/commons/api/infraestructure/configs/LanguageConfig.java
+++ backend/auth-service/src/main/java/com/smartjob/authservice/commons/api/infraestructure/configs/LanguageConfig.java
@@ -1,4 +1,43 @@
 package com.smartjob.authservice.commons.api.infraestructure.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.Validator;
+
+import static com.smartjob.authservice.commons.api.infraestructure.configs.Constants.SEPARATE;
+
+@Configuration
+@RefreshScope
 public class LanguageConfig {
+    @Value("${smartjob.language}")
+    String language;
+
+    @Bean
+    @RefreshScope
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:"+"messages"+SEPARATE+language);
+        messageSource.setCacheSeconds(10);
+        return messageSource;
+    }
+
+    public LocalValidatorFactoryBean validator()
+    {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+
+    public Validator getValidator()
+    {
+        return validator();
+    }
+
 }
