92eeadba0dd0d26d90371af5af851f14c28d4e72
Gustavo Israel Londa <gustavo.londa@inovtech.com.mx>
2024-08-16 23:17:35 -0500

    Se agrego el modelo del mensaje de error

--- backend/auth-service/src/main/java/com/smartjob/authservice/commons/api/domains/data/BaseException.java
+++ backend/auth-service/src/main/java/com/smartjob/authservice/commons/api/domains/data/BaseException.java
@@ -1,4 +1,16 @@
 package com.smartjob.authservice.commons.api.domains.data;
 
-public class BaseException {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseException extends Exception {
+    private String message;
+    private String module;
+    private Exception exception;
 }
