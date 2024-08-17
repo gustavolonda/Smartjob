368bc57bf4234ad35ab652df913ac0d3ff10745c
Gustavo Israel Londa <gustavo.londa@inovtech.com.mx>
2024-08-17 00:51:19 -0500

    Se implemento el servicio  del usuario

--- backend/auth-service/src/main/java/com/smartjob/authservice/user/domains/services/UserService.java
+++ backend/auth-service/src/main/java/com/smartjob/authservice/user/domains/services/UserService.java
@@ -1,4 +1,35 @@
 package com.smartjob.authservice.user.domains.services;
 
-public class UserService {
+import com.smartjob.authservice.commons.api.domains.data.BaseException;
+import com.smartjob.authservice.commons.api.domains.services.EndPointServiceImpl;
+import com.smartjob.authservice.user.infraestructure.entities.UserEntity;
+import com.smartjob.authservice.user.infraestructure.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class UserService extends EndPointServiceImpl<UserEntity, String> {
+    public static final String TAG = UserService.class.getSimpleName();
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public JpaRepository<UserEntity, String> getDao() {
+        return this.repository;
+    }
+
+    @Override
+    public UserEntity statusChangeDelete(UserEntity entity) {
+        return null;
+    }
+
+    @Override
+    public String nameModule() {
+        return TAG;
+    }
+
+    @Override
+    public BaseException validate(UserEntity entity) {
+        return null;
+    }
 }
