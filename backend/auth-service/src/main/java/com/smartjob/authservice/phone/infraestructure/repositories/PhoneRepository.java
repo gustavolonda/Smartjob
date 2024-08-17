c9aa84f9d7a8af17e671453e7221ca37192bd43b
Gustavo Israel Londa <gustavo.londa@inovtech.com.mx>
2024-08-17 00:37:11 -0500

    Se implemento el respositorio  del telefono

--- backend/auth-service/src/main/java/com/smartjob/authservice/phone/infraestructure/repositories/PhoneRepository.java
+++ backend/auth-service/src/main/java/com/smartjob/authservice/phone/infraestructure/repositories/PhoneRepository.java
@@ -1,4 +1,9 @@
 package com.smartjob.authservice.phone.infraestructure.repositories;
 
-public interface PhoneRepository {
+import com.smartjob.authservice.phone.infraestructure.entities.PhoneEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhoneRepository extends JpaRepository<PhoneEntity, String> {
 }
