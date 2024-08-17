e6579a4f8b4798fbd8f9e4419fc244433ca2b3b8
Gustavo Israel Londa <gustavo.londa@inovtech.com.mx>
2024-08-17 00:37:58 -0500

    Se implemento el servicio  del telefono

--- backend/auth-service/src/main/java/com/smartjob/authservice/phone/domains/services/PhoneService.java
+++ backend/auth-service/src/main/java/com/smartjob/authservice/phone/domains/services/PhoneService.java
@@ -1,4 +1,37 @@
 package com.smartjob.authservice.phone.domains.services;
 
-public class PhoneService {
+import com.smartjob.authservice.commons.api.domains.data.BaseException;
+import com.smartjob.authservice.commons.api.domains.services.EndPointServiceImpl;
+import com.smartjob.authservice.phone.infraestructure.entities.PhoneEntity;
+import com.smartjob.authservice.phone.infraestructure.repositories.PhoneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PhoneService extends EndPointServiceImpl<PhoneEntity, String> {
+    public static final String TAG = PhoneService.class.getSimpleName();
+    @Autowired
+    private PhoneRepository repository;
+
+    @Override
+    public JpaRepository<PhoneEntity, String> getDao() {
+        return this.repository;
+    }
+
+    @Override
+    public PhoneEntity statusChangeDelete(PhoneEntity entity) {
+        entity.setActive(Boolean.FALSE);
+        return entity;
+    }
+
+    @Override
+    public String nameModule() {
+        return TAG;
+    }
+
+    @Override
+    public BaseException validate(PhoneEntity entity) {
+        return null;
+    }
 }
