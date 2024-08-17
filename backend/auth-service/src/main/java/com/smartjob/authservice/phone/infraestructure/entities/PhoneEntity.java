4183e7d716e4ffa0032bc34fef51d4191add05a6
Gustavo Israel Londa <gustavo.londa@inovtech.com.mx>
2024-08-17 00:36:18 -0500

    Se implemento la entidad del telefono

--- backend/auth-service/src/main/java/com/smartjob/authservice/phone/infraestructure/entities/PhoneEntity.java
+++ backend/auth-service/src/main/java/com/smartjob/authservice/phone/infraestructure/entities/PhoneEntity.java
@@ -1,4 +1,33 @@
 package com.smartjob.authservice.phone.infraestructure.entities;
+import com.smartjob.authservice.commons.api.infraestructure.entities.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.GenericGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public class PhoneEntity {
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "phones")
+public class PhoneEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2",
+                    strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", columnDefinition = "text")
+    private String id;
+    private String number;
+    @Column(name = "city_code")
+    private String cityCode;
+    @Column(name = "country_code")
+    private String countryCode;
+
 }
