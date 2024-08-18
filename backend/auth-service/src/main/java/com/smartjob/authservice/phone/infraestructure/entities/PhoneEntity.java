19bca897eb2008a3fd0a707f2f488f4b523eb047
Gustavo Israel Londa <gustavo.londa@inovtech.com.mx>
2024-08-18 03:39:12 -0500

    Arreglo error con la relaccion

--- backend/auth-service/src/main/java/com/smartjob/authservice/phone/infraestructure/entities/PhoneEntity.java
+++ backend/auth-service/src/main/java/com/smartjob/authservice/phone/infraestructure/entities/PhoneEntity.java
@@ -8,13 +8,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "phones")
-public class PhoneEntity extends BaseEntity {
+public class  PhoneEntity extends BaseEntity implements Serializable {
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2",
@@ -26,8 +28,8 @@ public class PhoneEntity extends BaseEntity {
     private String cityCode;
     @Column(name = "country_code")
     private String countryCode;
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "user_id")
-    private UserEntity userEntity;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "user_id", nullable=false)
+    private UserEntity user;
 
 }
